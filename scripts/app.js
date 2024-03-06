6a75d4f7a5ed952891deaa5fca8877a01e50b03a
amirhosein keivani <amirhosein8292@gmail.com>
2024-03-06 21:59:39 +0330

    coding js of resume section

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,17 @@
 const navToggleIcon = document.querySelector(".nav__toggle-icon");
 const menu = document.querySelector(".menu");
 const cover = document.querySelector(".cover");
+const resumeListItems = document.querySelectorAll(".resume-list__item");
 navToggleIcon.addEventListener("click", () => {
   navToggleIcon.classList.toggle("nav__toggle-icon-open");
   menu.classList.toggle("menu--open");
   cover.classList.toggle("cover--show");
+});
+resumeListItems.forEach((resumeListItem) => {
+  resumeListItem.addEventListener("click", function () {
+    document
+      .querySelector(".resume-list__item--active")
+      .classList.remove("resume-list__item--active");
+    this.classList.add("resume-list__item--active");
+  });
 });
