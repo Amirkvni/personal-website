c0def0ef6b2464ff15be420594a93010228f0bdf
amirhosein keivani <amirhosein8292@gmail.com>
2024-03-08 16:37:53 +0330

    coding Intersection Observer API js file

--- scripts/app.js
+++ scripts/app.js
@@ -3,6 +3,9 @@ const menu = document.querySelector(".menu");
 const cover = document.querySelector(".cover");
 const resumeListItems = document.querySelectorAll(".resume-list__item");
 const portfolioListItems = document.querySelectorAll(".portfolio-list__item");
+const menuItems = document.querySelectorAll(".menu__item");
+const sections = document.querySelectorAll("main > section");
+
 navToggleIcon.addEventListener("click", function () {
   this.classList.toggle("nav__toggle-icon--open");
   menu.classList.toggle("menu--open");
@@ -16,17 +19,17 @@ function navigationTabsInit(
 ) {
   listItems.forEach((listItem) => {
     listItem.addEventListener("click", function () {
-      document
-        .querySelector(`.${listItemActiveClass}`)
-        .classList.remove(listItemActiveClass);
-      document
-        .querySelector(`.${contentItemShowClass}`)
-        .classList.remove(contentItemShowClass);
+      removeActiveClass(listItemActiveClass);
+      removeActiveClass(contentItemShowClass);
       this.classList.add(listItemActiveClass);
       let contentId = this.getAttribute("data-content-id");
       document.querySelector(contentId).classList.add(contentItemShowClass);
     });
   });
+}
+
+function removeActiveClass(className) {
+  document.querySelector(`.${className}`).classList.remove(className);
 }
 
 navigationTabsInit(
@@ -39,3 +42,40 @@ navigationTabsInit(
   "portfolio-list__item--active",
   "portfolio-content--show"
 );
+
+const observer = new IntersectionObserver(observerHandler, {
+  threshold: 0.5,
+});
+
+function observerHandler(allSections) {
+  allSections.map((section) => {
+    let sectionClassName = section.target.className;
+    let sectionMenuItem = document.querySelector(
+      `.menu__item[data-section=${sectionClassName}]`
+    );
+    if (section.isIntersecting) {
+      sectionMenuItem.classList.add("menu__link--active");
+    } else {
+      sectionMenuItem.classList.remove("menu__link--active");
+    }
+  });
+}
+
+sections.forEach((section) => {
+  observer.observe(section);
+});
+menuItems.forEach((item) => {
+  item.addEventListener("click", function (e) {
+    e.preventDefault();
+    removeActiveClass("menu__link--active");
+    item.classList.add("menu__link--active");
+
+    let sectionClass = item.getAttribute("data-section");
+    let sectionOffsetTop = document.querySelector(`.${sectionClass}`).offsetTop;
+
+    window.scrollTo({
+      top: sectionOffsetTop - 130,
+      behavior: "smooth",
+    });
+  });
+});
